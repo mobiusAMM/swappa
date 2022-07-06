1d244f9d0259e37c7a6499eefd2a393f280449a1
Dylan Mooers <dylmooers@gmail.com>
2022-07-06 08:14:18 -0700

    add pUSDC and asUSDC

--- src/cli/generatePairs.ts
+++ src/cli/generatePairs.ts
@@ -62,6 +62,8 @@ async function main() {
   const pairs = await manager.reinitializePairs([
     ...tokenWhitelist,
     "0x37f750B7cC259A2f741AF45294f6a16572CF5cAd",
+    "0xcC82628f6A8dEFA1e2B0aD7ed448bef3647F7941",
+    "0xCD7D7Ff64746C1909E44Db8e95331F9316478817",
   ]);
   await Promise.all(pairs.map((p) => p.loadLpAddress()));
   const descriptors = manager.getPairDescriptors();
